2719f0b375189d8a7cf0308993ee819b0ad99854
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-10-12 01:01:08 +0530

    dbConnection

---
+++ src/lib/dbConnet.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected ?: number
+}
+
+const connetion : ConnectionObject ={}
+
+
+
+async function dbConnect():Promise<void> {
+    if(connetion.isConnected){
+        console.log("already connected")
+        return
+    }
+    try {
+       const db= await mongoose.connect(process.env.MONGODB_URI || "" )
+       connetion.isConnected = db.connections[0].readyState
+       console.log( "db connected success")
+    } catch (err) {
+        
+        console.log("db connection fail",err)
+        process.exit(1)
+    }
+}
+
+export default dbConnect;

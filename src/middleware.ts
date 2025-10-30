9e6d94af4cf7dabe5a6a3c6c44ddcf35ef1adc23
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-10-31 00:22:28 +0530

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,21 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
+import { NextRequest,NextResponse } from 'next/server'
+ export { default } from "next-auth/middleware"
+ import { getToken } from "next-auth/jwt"
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+
+    const token = await getToken({req:request})
+    const url =request.nextUrl
+
+    if (token && (
+        url.pathname.startsWith('/sign-in') ||
+        url.pathname.startsWith('/sign-up') ||
+        url.pathname.startsWith('/') ||
+        url.pathname.startsWith('/verify') ||
+    )) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))      
+    }
+
   return NextResponse.redirect(new URL('/home', request.url))
 }
  

5e1336447cf2cf91e5ec2c98f9f4212b1cba513e
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-10-30 23:59:22 +0530

    next auth middleware

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -55,11 +55,25 @@ export const authOptions:NextAuthOptions ={
     ],
     callbacks: {
         
-        async session({ session, user, token }) {
-          return session
+        async jwt({ token, user }) {
+          
+          if (user) {
+            token._id = user._id?.toString()
+            token.isVerified = user.isVerified;
+            token.isAcceptingMessages = user.isAcceptingMessages;
+            token.username = user.username
+            
+          }
+            return token
         },
-        async jwt({ token, user, account, profile, isNewUser }) {
-          return token
+        async session({ session, token }) {
+            if (token) {
+                session.user._id = token._id;
+                session.user.isVerified =token.isVerified;
+                session.user.isAcceptingMessages =token.isAcceptingMessages
+                session.user.username = token.username
+            }
+          return session
         }
     },
     pages:{

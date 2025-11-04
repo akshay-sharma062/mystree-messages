1b1987363d25fcfb8676c439fe8ca3510481b1e8
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-11-04 23:01:24 +0530

    auth provider complition

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,12 +11,17 @@ export async function middleware(request: NextRequest) {
         url.pathname.startsWith('/sign-in') ||
         url.pathname.startsWith('/sign-up') ||
         url.pathname.startsWith('/') ||
-        url.pathname.startsWith('/verify') ||
+        url.pathname.startsWith('/verify') 
     )) {
         return NextResponse.redirect(new URL('/dashboard', request.url))      
     }
 
-  return NextResponse.redirect(new URL('/home', request.url))
+    if (!token && url.pathname.startsWith('/dashboard')) {
+      return NextResponse.redirect(new URL('/home', request.url));
+    }
+  
+    // Otherwise, continue normally
+    return NextResponse.next();
 }
  
 // See "Matching Paths" below to learn more

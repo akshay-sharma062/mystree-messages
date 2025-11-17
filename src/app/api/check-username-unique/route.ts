3c141bba35baba828080a2e110fb95653ee1a457
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-11-18 00:21:38 +0530

    username validate

---
+++ src/app/api/check-username-unique/route.ts
@@ -0,0 +1,58 @@
+import dbConnect from "@/lib/dbConnet";
+import UserModel from "@/model/user.model";
+import { z} from "zod";
+import {userNameValidation} from "@/schemas/signUp.schema"
+
+
+const UsernameQuerySchema = z.object({
+    username : userNameValidation
+})
+
+
+export async function GET(request : Request) {
+    await dbConnect()
+    try {
+        const {searchParams} = new URL(request.url)
+        const queryPram = {
+            username : searchParams.get("username ")
+        }
+        const result = UsernameQuerySchema.safeParse(queryPram)
+        if (!result.success) {
+            const usernameErrors = result.error.format().username?._errors || []
+            return Response.json({
+                success:false;
+                message : "invalid query parameter"
+            },
+            {status:500}
+            )
+        }
+        const {username}=result.data
+
+        const existingVerifiedUser = await UserModel.findOne({username,isVerified:true})
+        if (existingVerifiedUser) {
+            return Response.json({
+                success:false;
+                message : "User alredy taken"
+            },
+            {status:500}
+            )
+        }
+        return Response.json({
+            success:true;
+            message : "User name is unique"
+        },
+        {status:200}
+        )
+
+    } catch (error) {
+        console.log("error checking user name ",error)
+        return Response.json({
+            success:false;
+            message : "errror checking usrename"
+        },
+        {status:500}
+        )
+        
+    }
+    
+}

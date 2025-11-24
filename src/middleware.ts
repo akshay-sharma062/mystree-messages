061d795c9edff42fda9392f82856783558c2bf59
Akshay Sharma <akshay.ku.sharma.062@gmail.com>
2025-11-24 23:13:13 +0530

    Remove comment about matching paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,7 +23,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next();
 }
  
-// See "Matching Paths" below to learn more
+
 export const config = {
   matcher: [
     '/sign-in',

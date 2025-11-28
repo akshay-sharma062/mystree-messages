c7658db00ff1d5208b61318bc26fa72c95251541
akshay-sharma062 <akshay.ku.sharma.062@gmail.com>
2025-11-29 00:26:09 +0530

    get and post request handeld

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -5,23 +5,114 @@ import UserModel from "@/model/user.model";
 import { User } from "next-auth";
 
 
-export async function Post(request:Request) {
-    try {
+export async function POST(request:Request) {
+
         await dbConnect()
         const session = await getServerSession(authOptions)
-
-
-    } catch (error) {
-        console.log("error acepting messages ", error)
+        const user : User  = session?.user as User
+        if(!session || !session.user){
+            return Response.json(
+                {
+                    success : false,
+                    message : "not Authenticated"
+                },
+                {
+                   status : 401
+                }
+            )
+        }
+        const userId = user._id
+        const {acceptMessages} = await request.json()
+        try {
+           const updatedUser = await UserModel.findByIdAndUpdate(
+                userId,
+                {isAceptingMessage:acceptMessages},
+                {new:true}
+            )
+            if (!updatedUser) {
+                return Response.json(
+                    {
+                        success : false,
+                        message : "failed to update user status to acepting messages"
+                    },
+                    {
+                       status : 401
+                    }
+                )
+            }
+            return Response.json(
+                {
+                    success : true,
+                    message : "sucessfully updated user status to acepting messages",
+                    updatedUser
+                },
+                {
+                   status : 200
+                }
+            )
+        } catch (error) {
+            console.log("failed to update user status to acepting messages ", error)
         return Response.json(
             {
                 success : false,
-                message : "user can not accept messages"
+                message : "failed to update user status to acepting messages"
             },
             {
                status : 500
             }
         )
-    }  
+        }
 }
 
+export async function GET(request:Request){
+   
+    await dbConnect()
+    const session = await getServerSession(authOptions)
+    const user : User  = session?.user as User
+    if(!session || !session.user){
+        return Response.json(
+            {
+                success : false,
+                message : "not Authenticated"
+            },
+            {
+               status : 401
+            }
+        )
+    }
+    const userId = user._id     
+   try {
+     const foundUser = await UserModel.findById(userId)
+     if (!foundUser) {
+         return Response.json(
+             {
+                 success : false,
+                 message : "User not found"
+             },
+             {
+                status : 404
+             }
+         )
+     }
+     return Response.json(
+         {
+             success : true,
+             isAceptingMessages: foundUser.isAceptingMessage
+         },
+         {
+            status : 200
+         }
+     )
+   } catch (error) {
+    console.log("error in getting status to acepting messages ", error)
+        return Response.json(
+            {
+                success : false,
+                message : "error in getting status to acepting messages"
+            },
+            {
+               status : 500
+            }
+        )
+   }
+}
